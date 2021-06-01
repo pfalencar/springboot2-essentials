70ea7fe1cf975d719266b8775812578ad5d3f2a5
pfalencar <patricia.alencar@etec.sp.gov.br>
2021-06-01 16:30:19 -0300

    Método GET no Controller

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -1,17 +1,27 @@
 package academy.devdojo.springboot2.service;
 
 import academy.devdojo.springboot2.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 //classe responsável pela regra de negócio, onde vamos fazer a lógica de negócios
 @Service
 public class AnimeService {
+    private List<Anime> animes = List.of(new Anime(1l, "DB2"), new Anime(2L, "Berserk"));
     //private final AnimeRepository animeRepository;
 
     //vc não vai colocar esse método na aplicação, senão todos os usuários poderão fazer a listagem do seu BD inteiro na aplicação
     public List<Anime> listAll() {
-        return List.of(new Anime(1L, "Hero"),new Anime(2L,"Bersek"));
+        return animes;
+    }
+
+    public Anime findById(long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
     }
 }
