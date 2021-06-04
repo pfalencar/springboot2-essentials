b9b207884a4df7f65c8dcdd4bcd3c5018aef3be0
pfalencar <patricia.alencar@etec.sp.gov.br>
2021-06-04 12:41:20 -0300

    Spring Data JPA

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -2,11 +2,22 @@ package academy.devdojo.springboot2.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//domínio representa o que temos do BD.
 
 @Data //gera getters, setters, equals, hashCode e toString
 @AllArgsConstructor //gera um construtor com todos esses atributos
-//domínio representa o que temos do BD.
+@NoArgsConstructor
+@Entity //solicita um construtor sem argumentos, por isso coloco o @NoArgsConstructor. É do padrão JPA.
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
